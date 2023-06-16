fb2a4de21182cccf13ca88267215f9b04892dc03
Ferstina Wady Scott <111440067+FerstinaScott@users.noreply.github.com>
2023-06-16 18:26:56 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -1,4 +1,5 @@
 # R code for uploading and visualizing Coprnicus data in R
+Info: https://land.copernicus.eu/global/content/sce-nhemi-product-s-npp-viirs-data-affected
 
 # install.packages("ncdf4")
 # install.packages("viridis")
@@ -12,9 +13,12 @@ library(patchwork)
 # Set the working directory
 setwd("c:/Users/ferstinawady.scott/Documents/lab")
 
+# Comparing the snow cover of 2021 and 2023
+
 snow2023 <- raster("c_gls_LST_202101171800_GLOBE_GEO_V1.2.1.nc")
 snow2023
 
 plot(snow2023)
 
-
+snow2021 <- raster("c_gls_SCE_202106090000_NHEMI_VIIRS_V1.0.1.nc")
+snow2021
