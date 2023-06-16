d18b54aed60d1a5419f3919d82b5902acb9a067d
Ferstina Wady Scott <111440067+FerstinaScott@users.noreply.github.com>
2023-06-16 16:33:49 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -1,1 +1,20 @@
+# R code for uploading and visualizing Coprnicus data in R
+
+# install.packages("ncdf4")
+# install.packages("viridis")
+library(ncdf4)
+library(raster)
+library(RStoolbox)
+library(viridis)
+library(ggplot2)
+library(patchwork)
+
+# Set the working directory
+setwd("c:/Users/ferstinawady.scott/Documents/lab")
+
+snow2023 <- raster("c_gls_LST_202101171800_GLOBE_GEO_V1.2.1.nc")
+snow2023
+
+plot(snow2023)
+
 
